cc9c48b1545ea515de7a70f36b428dc177d8635e
Ricardo Sandez <ricardosandez67@gmail.com>
2025-07-22 22:58:29 +0200

    InteractUI.tsx: Setup basic structure of the home page, added fonts and styles for the game title

--- src/components/UI/InteractUI.tsx
+++ src/components/UI/InteractUI.tsx
@@ -1,5 +1,38 @@
+import styles from 'styles/components/InteractUI.module.scss'
+
+const { interact_ui, right_area, game_title_area, start_message, main_menu, game_title } = styles
+
 const InteractUI = () => {
-  return <div>Interact UI here</div>
+  const getMenu = () => {
+    return (
+      <div className={main_menu}>
+        <button>
+          <span>Leaderboard</span>
+        </button>
+        <button>
+          <span>Select bird</span>
+        </button>
+      </div>
+    )
+  }
+
+  const getStartMessage = () => {
+    return <div>Press space or click to play</div>
+  }
+
+  return (
+    <div className={interact_ui}>
+      <div className={right_area}>
+        <div className={game_title_area}>
+          <div className={game_title}>Flappy Bird</div>
+        </div>
+
+        {getMenu()}
+      </div>
+
+      <div className={start_message}>{getStartMessage()}</div>
+    </div>
+  )
 }
 
 export default InteractUI
