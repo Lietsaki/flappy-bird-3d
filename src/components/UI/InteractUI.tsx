348349912c7dd8d71ac9997b4c56d6585beace4e
Ricardo Sandez <ricardosandez67@gmail.com>
2025-08-10 16:25:27 +0200

    Setup game over text and modal

--- src/components/UI/InteractUI.tsx
+++ src/components/UI/InteractUI.tsx
@@ -1,6 +1,6 @@
 import styles from 'styles/components/InteractUI.module.scss'
 import Button from './Button'
-import { playingAtom } from '../../store/store'
+import { gameOverAtom, playingAtom } from '../../store/store'
 import { useAtom } from 'jotai'
 import { useEffect, useState } from 'react'
 import { wait } from '../../helpers/helper_functions'
@@ -13,13 +13,29 @@ const {
   main_menu,
   game_title,
   exiting_start_message,
-  exiting_right_area
+  exiting_right_area,
+  central_area,
+  game_over_text,
+  exit,
+  game_over_modal_area,
+  game_over_modal_container,
+  game_over_modal_bg,
+
+  game_over_modal,
+  game_over_modal_text,
+  game_over_modal_number,
+  game_over_buttons
 } = styles
 
 const InteractUI = () => {
   const [playing] = useAtom(playingAtom)
+  const [gameOver] = useAtom(gameOverAtom)
   const [showingUI, setShowingUI] = useState(true)
   const [uiExiting, setUIExiting] = useState(false)
+
+  const [gameOverTextEntering, setGameOverTextEntering] = useState(false)
+  const [gameOverTextExiting, setGameOverTextExiting] = useState(false)
+  const [showingGameOverModal, setShowingGameOverModal] = useState(false)
 
   useEffect(() => {
     const updateUI = async () => {
@@ -34,8 +50,35 @@ const InteractUI = () => {
     updateUI()
   }, [playing])
 
+  useEffect(() => {
+    if (!gameOver) return
+
+    const animateGameOverText = async () => {
+      await wait(500)
+      setGameOverTextEntering(true)
+
+      await wait(1300)
+      setGameOverTextExiting(true)
+      setShowingGameOverModal(true)
+
+      await wait(500)
+      setGameOverTextEntering(false)
+      setGameOverTextExiting(false)
+    }
+
+    animateGameOverText()
+  }, [gameOver])
+
   const goToSelectBird = () => {
     return ''
+  }
+
+  const restartGame = () => {
+    console.log('restartGame')
+  }
+
+  const backToMenu = () => {
+    console.log('backToMenu')
   }
 
   const getMenu = () => {
@@ -69,8 +112,56 @@ const InteractUI = () => {
     return <span className={exiting_class}>Press space or click to play</span>
   }
 
+  const getCentralArea = () => {
+    if (!gameOver) return null
+
+    const getGameOverText = () => {
+      if (!gameOverTextEntering && !gameOverTextExiting) return null
+
+      return (
+        <div className={gameOverTextExiting ? `${game_over_text} ${exit}` : game_over_text}>
+          <div>Game</div>
+          <div>Over</div>
+        </div>
+      )
+    }
+
+    const getGameOverModal = () => {
+      if (!showingGameOverModal) return null
+
+      return (
+        <div className={game_over_modal_area}>
+          <div className={game_over_modal_container}>
+            <div className={game_over_modal_bg}>
+              <div className={game_over_modal}>
+                <div className={game_over_modal_text}>Score</div>
+                <div className={game_over_modal_number}>1</div>
+
+                <div className={game_over_modal_text}>Best</div>
+                <div className={game_over_modal_number}>1</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={game_over_buttons}>
+            <Button text="Restart" onClick={restartGame} />
+            <Button text="Main menu" onClick={backToMenu} />
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div className={central_area}>
+        {getGameOverText()}
+        {getGameOverModal()}
+      </div>
+    )
+  }
+
   return (
     <div className={interact_ui}>
+      {getCentralArea()}
       {getRightArea()}
 
       <div className={start_message}>{getStartMessage()}</div>
