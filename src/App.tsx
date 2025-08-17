d0b166d9600912c8ed43cf3a0ab9c8ca45d2b1ac
Ricardo Sandez <ricardosandez67@gmail.com>
2025-08-17 21:22:12 +0200

    Created CameraManagement in App.tsx for responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import * as THREE from 'three'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { OrthographicCamera } from '@react-three/drei'
 
 import InteractUI from './components/UI/InteractUI'
 import NewYork from './components/NewYork'
 import Bird from './components/Bird'
 import DebugGui from './components/debug/DebugGui'
+import { useEffect, useState } from 'react'
+
+const CameraManagement = () => {
+  const [zoom, setZoom] = useState(26)
+  const [cameraPosition, setCameraPosition] = useState<THREE.Vector3Tuple>([-25, 13, 0])
+
+  const { size } = useThree()
+
+  useEffect(() => {
+    const base_ratio = { h: 1100, z: 26 }
+
+    setZoom(base_ratio.z * (size.height / base_ratio.h))
+
+    if (size.width < 600) {
+      setCameraPosition([-28, 13, 0])
+    }
+  }, [size])
+
+  return (
+    <OrthographicCamera
+      makeDefault
+      near={-100}
+      far={200}
+      zoom={zoom}
+      position={cameraPosition}
+    ></OrthographicCamera>
+  )
+}
 
 const App = () => {
   return (
     <>
       <Canvas gl={{ toneMapping: THREE.LinearToneMapping, antialias: true }}>
-        <OrthographicCamera
-          makeDefault
-          near={-100}
-          far={200}
-          zoom={26}
-          position={[-25, 13, 0]}
-        ></OrthographicCamera>
-
+        <CameraManagement />
         <DebugGui />
         <NewYork />
         <Bird />
